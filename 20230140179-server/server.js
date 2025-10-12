ce9b726f6e0d26579a48e1b6ecc305fb523f15ae
adilaroisa <adilaroisa688@gmail.com>
2025-10-12 16:06:44 +0700

    tugas2

--- 20230140179-server/server.js
+++ 20230140179-server/server.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const PORT = 3001;
 
+// Middleware
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// Route utama
+app.get('/', (req, res) => {
+  res.send('Home Page for API');
+});
+
+// Import router dari folder routes
+const bookRoutes = require('./routes/books');
+app.use('/api/books', bookRoutes);
+
+// Middleware 404 (Not Found)
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
---
+++ TUGAS/Tugas2.md
@@ -0,0 +1,17 @@
+Tugas 2 CRUD Buku
+
+1. Create 
+Menambahkan data buku baru ke server dengan metode POST melalui Postman.
+![Create](./ss/Create.png)
+
+2. Update
+Mengupdate data buku yang sudah ada di server menggunakan metode PUT di Postman. 
+![Update](ss/Put.png)
+
+3. Delete
+Menghapus data buku dari server menggunakan metode DELETE di Postman.
+![Delete](ss/Delete.png)
+
+4. Read
+Menampilkan seluruh data buku dari server menggunakan metode GET di Postman. 
+![Read](ss/Read.png)
